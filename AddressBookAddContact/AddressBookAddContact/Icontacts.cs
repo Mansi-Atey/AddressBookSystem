30f382b79e00196d8e9b8218c81510fbf49ee7ea
Mansi-Atey <mansiatey31@gmail.com>
2021-03-09 20:02:52 +0530

    [mansi]ADD: a new contact to address book

---
+++ AddressBookAddContact/AddressBookAddContact/Icontacts.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBookAddContact
+{
+    interface Icontacts
+    {
+        public void AddContact(string firstName, string lastName, string address, string city, string state, string email, int zip, long phoneNumber);
+    }
+}
