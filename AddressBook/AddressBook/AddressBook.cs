247af8afa81b57721b73313675591672e67bf411
Mansi-Atey <mansiatey31@gmail.com>
2021-03-09 19:59:07 +0530

    [mansi]ADD: create a contact in address book

---
+++ AddressBook/AddressBook/AddressBook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class AddressBook
+    {
+        static void Main(string[] args)
+        {
+
+            contacts cp = new contacts("Mansi", "Atey", "NIA", "Maharashtra", "Mumbai", 400058, "8899332211");
+            Console.WriteLine(cp.toString());
+        }
+    }
+}
---
+++ AddressBook/AddressBook/contacts.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AddressBook
+{
+    class contacts
+    {
+        public string firstName;
+        public string lastName;
+        public string address;
+        public string state;
+        public string city;
+        public int zipcode;
+        public string contactNumber;
+    public contacts(string firstName, string lastName, string address, string state, string city, int zipcode, string contactNumber)
+        {
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.address = address;
+            this.state = state;
+            this.city = city;
+            this.zipcode = zipcode;
+            this.contactNumber = contactNumber;
+        }
+        public string toString()
+        {
+            return "First Name:" + firstName + "\nLast Name:" + lastName + "\nAddress:" + address + "\nState:" + state + "\nCity:" + city + "\nZipCode:" + zipcode + "\nContactNumber:" + contactNumber;
+        }
+}
+}
+
